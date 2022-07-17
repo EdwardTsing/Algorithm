0585530cc6c26ec58912ea819322bd5571acf44f
XionLi <109472934+XionLi@users.noreply.github.com>
2022-07-17 20:19:25 +0200

    Time: 26 ms (10.00%), Space: 74.6 MB (6.06%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -3,8 +3,29 @@ import java.util.Arrays;
 public class Solution {
 
     public int findKthLargest(int[] nums, int k) {
-        int len = nums.length;
-        Arrays.sort(nums);
-        return nums[len - k];
+        k = nums.length - k;
+        return quickSelect(nums, k,  0, nums.length - 1);
+    }
+    
+    private int quickSelect(int[] nums, int k, int l, int r) {
+        int pivot = nums[r], p = l;
+        for (int i = l; i < r; i++) {
+            if (nums[i] <= pivot) {
+                int temp = nums[i];
+                nums[i] = nums[p];
+                nums[p] = temp;
+                p++;
+            }
+        }
+        int temp = nums[r];
+        nums[r] = nums[p];
+        nums[p] = temp;
+        if (k < p) {
+            return quickSelect(nums, k, l, p - 1);
+        } else if (k > p) {
+            return quickSelect(nums, k, p + 1, r);
+        } else {
+            return nums[k];
+        }
     }
 }
